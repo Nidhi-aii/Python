ddf30568b7cddbd9f3f721a2ddbe05cb33321b22
Nidhi Aii <nidhinnadiger@gmail.com>
2025-07-24 11:38:30 +0530

    Initial commit: added Python exercises

---
+++ 03_print_formatting.py
@@ -0,0 +1,151 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "0721f0f9-6f9c-485e-b405-28b850658e04",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "My name is : Barfi \n",
+      " And my age is :  8\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = 'Barfi'\n",
+    "age = 8\n",
+    "print('My name is :' ,name, '\\n' ,'And my age is : ' , age)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "id": "6cfb06cc-7b46-4f0a-9b7b-e2a21a638a7e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "My name is Nikki and I am 25 years old.\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = \"Nikki\"\n",
+    "age = 25\n",
+    "print(\"My name is {} and I am {} years old.\".format(name, age))\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "c7088e6e-794c-4b97-9f17-5da4bc79a202",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The total cost is ₹123.45\n"
+     ]
+    }
+   ],
+   "source": [
+    "price = 123.45\n",
+    "print(f\"The total cost is ₹{price:.2f}\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "6ebffa50-0fa3-4a58-9502-8ab31d3233b9",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Nikki\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = \"Nikki\"\n",
+    "print(f\"Hello {name}\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 21,
+   "id": "18a680e3-d8b2-4827-90e2-86dd0c87e50c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The total cost is $1234.5\n"
+     ]
+    }
+   ],
+   "source": [
+    "price = 1234.456\n",
+    "print('The total cost is ${:.1f}'.format(price))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 36,
+   "id": "37886076-b300-479d-af36-2b860cdb7ab5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Total = 15\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 10\n",
+    "b = 5 \n",
+    "print(f'Total = {a+b}')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "54312dd6-b599-4dc4-94cc-065b64a2d909",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
